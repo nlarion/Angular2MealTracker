97fad9f02b1baa2083362e13e3347dbff798474c
Neil Larion <neil.larion@gmail.com>
2016-03-25 11:25:10 -0700

    Split meal-display and meal-list into seperate files

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,66 +1,6 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
-import { MealDetails } from './meal-details.component';
-
-@Component({
-  selector: 'meal-display',
-  inputs: ['meal'],
-  template: `
-    <h3>{{ meal.name }}</h3>
-  `
-})
-
-export class MealComponent {
-  public meal: Meal;
-}
-
-@Component({
-  selector: 'meal-list',
-  inputs: ['mealList'],
-  outputs: ['onMealSelect'],
-  directives:[MealComponent, MealDetails],
-  template: `
-  <select (change)="onChange($event.target.value)">
-    <option value="all" selected="selected">Show All</option>
-    <option value="low">Show Low</option>
-    <option value="notLow" >Show Not Low</option>
-  </select>
-  <meal-display *ngFor="#currentMeal of mealList"
-  (click)="mealClicked(currentMeal)"
-  [class.selected]="currentMeal === selectedMeal"
-  [meal]="currentMeal"
-  [class.zBeer]="currentMeal.price < 5">
-  </meal-display>
-  <meal-details *ngIf="selectedMeal" [meal]="selectedMeal">
-  </meal-details>
-  <edit-meal-details *ngIf="selectedMeal" [meal]="selectedMeal">
-  </edit-meal-details>
-  <new-meal (onSubmitNewMeal)="createMeal($event)"></new-meal>
-  `
-})
-export class MealListComponent {
-  public mealList: Meal[];
-  public onMealSelect: EventEmitter<any>;
-  public selectedMeal: Meal;
-  public filterLow: string = "All";
-  public filterLowPints: string = "All";
-  constructor() {
-    this.onMealSelect = new EventEmitter();
-  }
-  mealClicked(clickedMeal: Meal): void {
-    console.log("child", clickedMeal);
-    this.selectedMeal = clickedMeal;
-    this.onMealSelect.emit(clickedMeal);
-  }
-  createMeal(holder: Array<any>): void {
-    this.mealList.push(
-      new Meal(holder[0], holder[1], holder[2], this.mealList.length)
-    );
-  }
-  onChange(filterOption) {
-    this.filterLow = filterOption;
-  }
-}
+import { MealListComponent } from './meal-list.component';
 
 @Component({
   selector: 'my-app',
--- app/meal-display.component.ts
+++ app/meal-display.component.ts
@@ -2,16 +2,13 @@ import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 
 @Component({
-  selector: 'meal-details',
+  selector: 'meal-display',
   inputs: ['meal'],
   template: `
-    <div>
-      <h4> Details: {{ meal.details }}</h4>
-      <h4> Calories: {{ meal.calories }}</h4>
-    <div>
+    <h3>{{ meal.name }}</h3>
   `
 })
 
-export class MealDetails {
+export class MealComponent {
   public meal: Meal;
 }
